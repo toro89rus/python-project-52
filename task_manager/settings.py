a2f877e84ba20725ab8e355b2facce216d7f5f7f
toro <timofey89rus@gmail.com>
2025-08-25 18:23:28 +0300

    add task-manager-2c0i.onrender.com to allowed_hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,12 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["webserver", "localhost", "127.0.0.1", "render.com"]
+ALLOWED_HOSTS = [
+    "webserver",
+    "localhost",
+    "127.0.0.1",
+    "task-manager-2c0i.onrender.com",
+]
 
 
 # Application definition
