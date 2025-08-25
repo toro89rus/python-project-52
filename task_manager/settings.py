bcbec18d2e4e8ccf6439795e37e556292cbc1b2c
toro <timofey89rus@gmail.com>
2025-08-25 18:16:15 +0300

    add render.com to allowed_hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["webserver", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["webserver", "localhost", "127.0.0.1", "render.com"]
 
 
 # Application definition
