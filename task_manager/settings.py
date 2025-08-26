0771eb6c37bdc2fdd3e596941ddbb9428a1db669
toro <timofey89rus@gmail.com>
2025-08-26 16:47:14 +0300

    fix lint mistakes(unsorted imports)

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 import dj_database_url
+
 from task_manager.config import DATABASE_URL, SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
