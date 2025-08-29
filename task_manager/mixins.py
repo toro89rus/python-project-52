b9e0d68952089c867616ec6f4fd95e2b7b3f976f
toro <timofey89rus@gmail.com>
2025-08-29 22:04:49 +0300

    fix custom mixins(loginreqiured, ownprofile) resolution order

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -6,16 +6,27 @@ from django.utils.translation import gettext_lazy as _
 
 
 class LoginRequiredWithMessageMixin(LoginRequiredMixin):
+
+    def get_redirect_field_name(self):
+        """
+        Override this method to override the redirect_field_name attribute.
+        """
+        return None
+
     def dispatch(self, request, *args, **kwargs):
         if not self.request.user.is_authenticated:
             messages.error(request, _("You're not authorised. Please login"))
+            return redirect(reverse_lazy("login"))
         return super().dispatch(request, *args, **kwargs)
 
 
 class OwnProfileMixin(UserPassesTestMixin):
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.test_func():
+            messages.error(request, _("You can't edit other user"))
+            return redirect(reverse_lazy("users_index"))
+        return super().dispatch(request, *args, **kwargs)
+
     def test_func(self):
         return self.request.user.id == self.get_object().id
-
-    def handle_no_permission(self):
-        messages.error(self.request, _("You can't edit other user"))
-        return redirect(reverse_lazy("users_index"))
