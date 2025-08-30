950cbbacd6b4b40660c53a2820950481d9cc160f
toro <timofey89rus@gmail.com>
2025-08-30 14:13:47 +0300

    add __str__ to status model

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -5,3 +5,6 @@ class Status(models.Model):
     name = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
