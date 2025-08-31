a657ed870066778b73dcb299539b6d9df5b449ae
toro <timofey89rus@gmail.com>
2025-08-31 10:44:54 +0300

    fix permissiondeniedmixin

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -7,12 +7,6 @@ from django.utils.translation import gettext_lazy as _
 
 class LoginRequiredWithMessageMixin(LoginRequiredMixin):
 
-    def get_redirect_field_name(self):
-        """
-        Override this method to override the redirect_field_name attribute.
-        """
-        return None
-
     def dispatch(self, request, *args, **kwargs):
         if not self.request.user.is_authenticated:
             messages.error(request, _("You're not authorised. Please login"))
@@ -20,13 +14,28 @@ class LoginRequiredWithMessageMixin(LoginRequiredMixin):
         return super().dispatch(request, *args, **kwargs)
 
 
-class OwnProfileMixin(UserPassesTestMixin):
+class PermissionDeniedMixin(UserPassesTestMixin):
+    error_message = "You don't have permission to do this"
+    redirect_url = "main"
 
     def dispatch(self, request, *args, **kwargs):
         if not self.test_func():
-            messages.error(request, _("You can't edit other user"))
-            return redirect(reverse_lazy("users_index"))
+            messages.error(request, _(self.error_message))
+            return redirect(reverse_lazy(self.redirect_url))
         return super().dispatch(request, *args, **kwargs)
+
+
+class OwnProfileMixin(PermissionDeniedMixin):
+    error_message = "You can't edit other user"
+    redirect_url = "users_index"
 
     def test_func(self):
         return self.request.user.id == self.get_object().id
+
+
+class OwnTaskMixin(PermissionDeniedMixin):
+    error_message = "Only author have permission to delete task"
+    redirect_url = "tasks_index"
+
+    def test_func(self):
+        return self.request.user.id == self.get_object().author_id
