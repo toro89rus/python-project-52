d1447d5c4b80ba81891a71f92ef6e440c12458c3
toro <timofey89rus@gmail.com>
2025-09-04 16:37:27 +0300

    move static folder to task_manager/

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR / "static_files"
+STATIC_ROOT = BASE_DIR / "task_manager" / "static_files"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
