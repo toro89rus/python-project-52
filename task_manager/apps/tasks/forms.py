bea183668f7a33e20881f0ee73912dea7d72d0ab
toro <timofey89rus@gmail.com>
2025-09-04 19:43:07 +0300

    fix linting mistakes

--- task_manager/apps/tasks/forms.py
+++ task_manager/apps/tasks/forms.py
@@ -3,8 +3,8 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 
 from task_manager.apps.labels.models import Label
-from task_manager.apps.tasks.models import Task
 from task_manager.apps.statuses.models import Status
+from task_manager.apps.tasks.models import Task
 from task_manager.apps.users.models import User
 
 
