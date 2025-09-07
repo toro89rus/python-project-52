221bc37ce133d3ea3751c7ea46bbf2ab8391ac35
toro <timofey89rus@gmail.com>
2025-09-07 12:50:58 +0300

    fix mixins flash messages mixup due to same atrributes names

--- task_manager/apps/core/mixins.py
+++ task_manager/apps/core/mixins.py
@@ -8,54 +8,55 @@ from task_manager.apps.core import text_constants
 
 
 class UserPassesTestWithMessageMixin(UserPassesTestMixin):
-    error_message = "You don't have permission to do this"
+    permission_denied_message = "You don't have permission to do this"
     redirect_url = "main"
 
     def dispatch(self, request, *args, **kwargs):
         if not self.test_func():
-            messages.error(request, self.error_message)
+            messages.error(request, self.permission_denied_message)
             return redirect(reverse_lazy(self.redirect_url))
         return super().dispatch(request, *args, **kwargs)
 
 
 class UserIsProfileOwnerMixin(UserPassesTestWithMessageMixin):
-    error_message = text_constants.USER_PERMISSION_DENIED
+    permission_denied_message = text_constants.USER_PERMISSION_DENIED
     redirect_url = "users_index"
 
     def test_func(self):
+        print("test_func called")
         return self.request.user.id == self.get_object().id
 
 
 class UserIsTaskAuthorMixin(UserPassesTestWithMessageMixin):
-    error_message = text_constants.TASK_PERMISSION_DENIED
+    permission_denied_message = text_constants.TASK_PERMISSION_DENIED
     redirect_url = "tasks_index"
 
     def test_func(self):
         return self.request.user.id == self.get_object().author_id
 
 
-class RedirectOnRestrictedDeleteMixin:
-    error_message = "You can't delete this"
+class RedirectOnRestrictedDeleteMixin():
+    restict_message = "You can't delete this"
     redirect_url = "main"
 
     def form_valid(self, form):
         try:
             return super().form_valid(form)
         except RestrictedError:
-            messages.error(self.request, self.error_message)
+            messages.error(self.request, self.restict_message)
             return redirect(reverse_lazy(self.redirect_url))
 
 
 class RestrictStatusDeleteMixin(RedirectOnRestrictedDeleteMixin):
-    error_message = text_constants.STATUS_RESTRICT_DELETE
+    restict_message = text_constants.STATUS_RESTRICT_DELETE
     redirect_url = "statuses_index"
 
 
 class RestrictUserDeleteMixin(RedirectOnRestrictedDeleteMixin):
-    error_message = text_constants.USER_RESTRICT_DELETE
+    restict_message = text_constants.USER_RESTRICT_DELETE
     redirect_url = "users_index"
 
 
 class RestrictLabelDeleteMixin(RedirectOnRestrictedDeleteMixin):
-    error_message = text_constants.LABEL_RESTRICT_DELETE
+    restict_message = text_constants.LABEL_RESTRICT_DELETE
     redirect_url = "labels_index"
